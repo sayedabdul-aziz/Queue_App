406e9fa4e43af891c929f122609b7b45239df497
Sayed Abdul-Aziz <71665268+sayedabdul-aziz@users.noreply.github.com>
2021-12-12 01:20:13 +0200

    Add files via upload

---
+++ src/queue_app/Queue_App.java
@@ -0,0 +1,12 @@
+package queue_app;
+
+
+public class Queue_App {
+
+    public static void main(String[] args) {
+       menu frame = new menu();
+       frame.setVisible(true);
+        
+    }
+
+}
